c26dfdc2d093c483fbb38d447128eaae30c2c732
Wash1se <damirga2006@gmail.com>
2023-03-13 00:10:11 +0300

    refactored send response to django func and edited response messages

--- main.py
+++ main.py
@@ -5,7 +5,28 @@ import time
 import asyncio
 import redis
 import requests
+import aiohttp
 
+
+async def async_get_request(url:str, proxy:str=""):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, proxy=proxy) as response:
+            return await response
+
+
+async def async_post_request(url:str, proxy:str="", data:dict={}):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, proxy=proxy, data=data) as response:
+            return await response
+
+django_url = 'http://localhost:8001/api/fastapi/'
+
+async def send_response_to_django(tg_id:str, message:str):
+    global django_url
+    await async_post_request(
+        url=django_url,
+        data={"tg_id":tg_id, "message": message}
+    )
 
 router = APIRouter()
 app = FastAPI(title="lzttgbot")
@@ -15,7 +36,7 @@ Redis = redis.Redis(
     db=0,
     )
 
-django_url = 'http://localhost:8001'
+
 
 ##########
 # WORKER #
@@ -25,9 +46,9 @@ async def worker(tg_id:int, redis):
     while True:
         if Redis.get(str(tg_id)).decode('utf-8')!='True':
             return
-        response = requests.post(
-                url=django_url+'/api/fastapi/',
-                data={"tg_id":tg_id, "message": f"is working {Redis.get(str(tg_id)).decode('utf-8')}"}
+        await send_response_to_django(
+            tg_id,
+            f"is working {Redis.get(str(tg_id)).decode('utf-8')}"
         )
         await asyncio.sleep(3)
 
@@ -43,26 +64,41 @@ class Data(BaseModel):
 async def start(data:Data):
     tg_id = data.tg_id
     global Redis
-    with Redis.pipeline() as pipe:
-        pipe.multi()
-        pipe.set(str(tg_id), 'True'.encode('utf-8'))
-        result = pipe.execute()
-    
-    asyncio.create_task(worker(tg_id, redis))
+    if Redis.get(str(tg_id)).decode('utf-8') == 'True':
+        await send_response_to_django(
+            tg_id=tg_id,
+            message="Скан уже идет"
+        )
+    else:
+        with Redis.pipeline() as pipe:
+            pipe.multi()
+            pipe.set(str(tg_id), 'True'.encode('utf-8'))
+            result = pipe.execute()
         
-    return {"result": result}
+        asyncio.create_task(worker(tg_id, redis))
+
+
 
 
 @router.post('/stop')
 async def stop(data:Data):
     tg_id = data.tg_id
     global Redis
-    with Redis.pipeline() as pipe:
-        pipe.multi()
-        pipe.set(str(tg_id), 'False'.encode('utf-8'))
-        result = pipe.execute()
-            
-    return {"result": result}
+    if Redis.get(str(tg_id)).decode('utf-8') != 'True':
+        await send_response_to_django(
+            tg_id=tg_id,
+            message="Скан и так не идет"
+        )
+    else:
+        with Redis.pipeline() as pipe:
+            pipe.multi()
+            pipe.set(str(tg_id), 'False'.encode('utf-8'))
+            result = pipe.execute()
+                
+        await send_response_to_django(
+            tg_id=tg_id,
+            message="Скан остановлен"
+        )
 
 
 
